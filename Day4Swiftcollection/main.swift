0aadfa1c49d65da79c2a01833c4e2eeade50d01f
Shivani <shivanidhiman1619@gmail.com>
2019-06-11 18:53:54 -0400

    [Shivani] :  Array

--- Day4Swiftcollection/main.swift
+++ Day4Swiftcollection/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World!")
+var a = [10,20,30,40,50,60,70,80,90,100]
+//print(a[1])
+//print(a[10])
+
+
+for x in a
+{
+    print(x)
+}
+
+a.append(110)
+a.append(120)
+//a[10] = 110
+
+print(a[10])
+
+print("----------------")
+print(a.capacity)
+print(a.count)
+
+if a.isEmpty
+{
+    print("Array is empty")
+}
+else{
+    print("Array contains some elements")
+}
+
+var x = Int()
+print(x)
+var country :[String]?
+country = [String]()
+
+var b : [String]?
+//b = [String]()
+if var c = country
+{
+    c.append("India")
+    c.append("Canada")
+    c.append("USA")
+    print("No. of countries : \(c.count)")
+}
+
+print("No. of countries : \(country!.count)")
+
 
