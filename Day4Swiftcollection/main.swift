30932f1d90fc31706d3450c4c521debebc627e1b
Shivani <shivanidhiman1619@gmail.com>
2019-06-11 18:23:36 -0400

    Initial Commit

---
+++ Day4Swiftcollection/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day4Swiftcollection
+//
+//  Created by MacStudent on 2019-06-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
